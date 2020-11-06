d5f16702c7933ecb11f8d9f26f5e814389cde2df
Toogii <dashdava@uw.edu>
2020-11-05 23:23:46 -0800

    Added FE API for Sign Up and Sign Ins

--- client/src/components/login.js
+++ client/src/components/login.js
@@ -1,6 +1,27 @@
 import React, { Component } from "react";
+import $ from 'jquery';
 
 export default class Login extends Component {
+
+    handleSignIn = () => {
+        console.log("Sending user info for sign in");
+
+        const newUserInfo = {
+            email: $(".email").val(),
+            password: $(".password").val() 
+        }
+
+        const data = {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(newUserInfo)
+        };
+        fetch("/api/sign_in", data)
+        .then(res => console.log(res));
+    }
+
     render() {
         return (
             <form>
@@ -8,12 +29,12 @@ export default class Login extends Component {
 
                 <div className="form-group">
                     <label>Email address</label>
-                    <input type="email" className="form-control" placeholder="Enter email" />
+                    <input type="email" className="form-control email" placeholder="Enter email" />
                 </div>
 
                 <div className="form-group">
                     <label>Password</label>
-                    <input type="password" className="form-control" placeholder="Enter password" />
+                    <input type="password" className="form-control password" placeholder="Enter password" />
                 </div>
 
                 <div className="form-group">
@@ -23,7 +44,7 @@ export default class Login extends Component {
                     </div>
                 </div>
 
-                <button type="submit" className="btn btn-light btn-block">Submit</button>
+                <button type="button" className="btn btn-light btn-block" onClick={ this.handleSignIn }>Sign In</button>
                 <p className="forgot-password text-right">
                     Forgot <a href="#">password?</a>
                 </p>
