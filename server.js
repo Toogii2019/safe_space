836689934561715f6c13f4a40bf8734291f73b1c
Toogii <dashdava@uw.edu>
2020-11-05 20:05:43 -0800

    Added html routes and apiRoutes

--- server.js
+++ server.js
@@ -11,13 +11,13 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
+require('./routes/apiRoutes')(app);
+require('./routes/htmlRoutes')(app);
+
 // Define API routes here
 
 // Send every other request to the React app
 // Define any API routes before this runs
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
