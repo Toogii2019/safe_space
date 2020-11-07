2711cdd1a08de1f7e2dfdc68e863c6eb821a67d2
Julee Butler <jiny2060@hotmail.com>
2020-11-07 14:26:36 -0800

    finish up code for api route backend server to link with front end data

--- client/src/components/PublicFeed.js
+++ client/src/components/PublicFeed.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import Avatar from './Avatar';
+import {posts} from '../utils/API'
 
 const useStyles = makeStyles({
   root: {
@@ -22,6 +23,11 @@ const useStyles = makeStyles({
 
 export default function SimpleCard() {
   const classes = useStyles();
+  
+  useEffect(() => { 
+    posts()
+    .then(res => localStorage.setItem("allposts", JSON.stringify(res.data)))
+  }, []);
 
   return (
     <Card className={classes.root}>
