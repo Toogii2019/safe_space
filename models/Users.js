b4c9edc3b7a93f8d7617f16c416442dae05dbfb0
Toogii <dashdava@uw.edu>
2020-11-06 22:24:34 -0800

    Making nickname and email unique in DB

--- models/Users.js
+++ models/Users.js
@@ -5,9 +5,11 @@ const Schema = mongoose.Schema;
 const UsersScema = new Schema({
   nickname: {
     type: String,
+    unique: true
   },
   email: {
     type: String,
+    unique: true
   },
   password: {
     type: String,
